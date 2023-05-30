648bf583179931ea1cb40ca66ebe0503142ec2e7
MichaelBengtsson <michael@krokedil.se>
2023-05-30 14:28:29 +0200

    Add method to convert prices from minor units

--- src/Base.php
+++ src/Base.php
@@ -47,12 +47,12 @@ class Base {
 	public function __construct( $config = array() ) {
 		$this->config = wp_parse_args( $config, $this->defaults );
 
-		$this->slug = $this->config['slug'];
+		$this->slug         = $this->config['slug'];
 		$this->price_format = $this->config['price_format'];
 	}
 
 	/**
-	 * Formats the price according to either the config provided or the price_format passed .
+	 * Formats the price from major units according to either the config provided or the price_format passed .
 	 *
 	 * @param int|float|string $price The price to be formated .
 	 * @param string|null      $price_format The price format to be used, either minor or major as a string .
@@ -73,12 +73,33 @@ class Base {
 	}
 
 	/**
+	 * Formats the price from minor units according to either the config provided or the price_format passed.
+	 *
+	 * @param int|float|string $price The price to be formated .
+	 * @param string|null      $price_format The price format to be used, either minor or major as a string .
+	 *
+	 * @return int|float
+	 */
+	public function format_price_from_minor( $price, $price_format = null ) {
+		$price_format   = $price_format ? $price_format : $this->price_format;
+		$decimal_points = $price_format === 'minor' ? 0 : 2;
+
+		if ( 'major' === $price_format ) {
+			$price = floatval( $price ) / 100;
+		}
+
+		$formated_price = wc_format_decimal( $price, $decimal_points );
+
+		return $price_format === 'minor' ? intval( $formated_price ) : floatval( $formated_price );
+	}
+
+	/**
 	 * Gets the prefix to use for the filter name.
 	 *
 	 * @return string
 	 */
 	public function get_filter_prefix() {
-		$slug = $this->slug;
+		$slug   = $this->slug;
 		$source = $this->filter_prefix;
 		return "{$slug}_{$source}";
 	}
