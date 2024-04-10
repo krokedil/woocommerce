2bd0c5237ac2d4749eacf726a8d0d798fdfc07e8
MichaelBengtsson <michael@krokedil.se>
2024-04-10 17:17:17 +0200

    Update StoreApiLineCoupon.php

--- src/StoreApi/StoreApiLineCoupon.php
+++ src/StoreApi/StoreApiLineCoupon.php
@@ -71,11 +71,12 @@ class StoreApiLineCoupon extends OrderLineData {
 		$this->set_type();
 		$this->set_product_url();
 		$this->set_image_url();
-		$this->set_compatability();
+		$this->set_compatibility();
 	}
 
 	/**
 	 * Function to set product name
+	 *
 	 * @return void
 	 */
 	public function set_name() {
@@ -84,6 +85,7 @@ class StoreApiLineCoupon extends OrderLineData {
 
 	/**
 	 * Function to set product sku
+	 *
 	 * @return void
 	 */
 	public function set_sku() {
@@ -92,6 +94,7 @@ class StoreApiLineCoupon extends OrderLineData {
 
 	/**
 	 * Function to set product quantity
+	 *
 	 * @return void
 	 */
 	public function set_quantity() {
@@ -100,6 +103,7 @@ class StoreApiLineCoupon extends OrderLineData {
 
 	/**
 	 * Function to set product unit price
+	 *
 	 * @return void
 	 */
 	public function set_unit_price() {
@@ -108,6 +112,7 @@ class StoreApiLineCoupon extends OrderLineData {
 
 	/**
 	 * Function to set product subtotal unit price
+	 *
 	 * @return void
 	 */
 	public function set_subtotal_unit_price() {
@@ -116,6 +121,7 @@ class StoreApiLineCoupon extends OrderLineData {
 
 	/**
 	 * Function to set product tax rate
+	 *
 	 * @return void
 	 */
 	public function set_tax_rate() {
@@ -124,6 +130,7 @@ class StoreApiLineCoupon extends OrderLineData {
 
 	/**
 	 * Function to set product total amount
+	 *
 	 * @return void
 	 */
 	public function set_total_amount() {
@@ -132,6 +139,7 @@ class StoreApiLineCoupon extends OrderLineData {
 
 	/**
 	 * Function to set product subtotal amount
+	 *
 	 * @return void
 	 */
 	public function set_subtotal_amount() {
@@ -140,6 +148,7 @@ class StoreApiLineCoupon extends OrderLineData {
 
 	/**
 	 * Function to set product total discount amount
+	 *
 	 * @return void
 	 */
 	public function set_total_discount_amount() {
@@ -148,6 +157,7 @@ class StoreApiLineCoupon extends OrderLineData {
 
 	/**
 	 * Abstract function to set product total discount tax amount
+	 *
 	 * @return void
 	 */
 	public function set_total_discount_tax_amount() {
@@ -156,6 +166,7 @@ class StoreApiLineCoupon extends OrderLineData {
 
 	/**
 	 * Function to set product total tax amount
+	 *
 	 * @return void
 	 */
 	public function set_total_tax_amount() {
@@ -164,6 +175,7 @@ class StoreApiLineCoupon extends OrderLineData {
 
 	/**
 	 * Function to set product subtotal tax amount
+	 *
 	 * @return void
 	 */
 	public function set_subtotal_tax_amount() {
@@ -172,6 +184,7 @@ class StoreApiLineCoupon extends OrderLineData {
 
 	/**
 	 * Function to set product type
+	 *
 	 * @return void
 	 */
 	public function set_type() {
@@ -182,6 +195,7 @@ class StoreApiLineCoupon extends OrderLineData {
 
 	/**
 	 * Function to set product url
+	 *
 	 * @return void
 	 */
 	public function set_product_url() {
@@ -190,6 +204,7 @@ class StoreApiLineCoupon extends OrderLineData {
 
 	/**
 	 * Function to set product image url
+	 *
 	 * @return void
 	 */
 	public function set_image_url() {
@@ -198,9 +213,10 @@ class StoreApiLineCoupon extends OrderLineData {
 
 	/**
 	 * Function to set product compatability
+	 *
 	 * @return void
 	 */
-	public function set_compatability() {
+	public function set_compatibility() {
 		$this->compatability = apply_filters( $this->get_filter_name( 'compatability' ), array(), $this->coupon );
 	}
 }
