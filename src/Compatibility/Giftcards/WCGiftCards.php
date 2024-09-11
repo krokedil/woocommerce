0988e23494c22e75c32f86c500fa217140295604
anyakro <anya@krokedil.se>
2024-09-11 18:01:31 +0200

    Add support for account balance gift cards

--- src/Compatibility/Giftcards/WCGiftCards.php
+++ src/Compatibility/Giftcards/WCGiftCards.php
@@ -19,13 +19,41 @@ class WCGiftCards extends AbstractGiftCardCompatibility {
 	public function get_cart_giftcards() {
 		$coupons = array();
 
-		foreach ( WC_GC()->cart->get_applied_gift_cards()['giftcards'] as $wc_giftcard ) {
-			$code   = $wc_giftcard['giftcard']->get_data()['code'];
-			$amount = $wc_giftcard['amount'] * -1;
+		// Check if the account gift card balance is used.
+		$use = isset( $post[ 'use_gift_card_balance' ] ) && 'on' === $post[ 'use_gift_card_balance' ] ? true : false;
+		if ( WC_GC()->account->use_balance() !== $use ) {
 
-			$coupons[] = $this->create_gift_card( "$this->name $code", $this->sku, $this->type, $amount );
+			$account_giftcard_balance = WC_GC()->giftcards->cover_balance( WC()->cart->get_total() );
+			if($account_giftcard_balance) {
+				$coupons = array_merge($coupons, $this->create_gift_cards($account_giftcard_balance['giftcards']));
+			}
 		}
 
+		$applied_giftcards = WC_GC()->cart->get_applied_gift_cards()['giftcards'];
+		if($applied_giftcards) {
+    		$coupons = array_merge($coupons, $this->create_gift_cards($applied_giftcards));
+		}
+
+		return $coupons;
+	}
+
+	/**
+	 * Create gift cards.
+	 *
+	 * @param array $giftcards The gift cards.
+	 *
+	 * @return OrderLineData[]
+	 */
+	private function create_gift_cards($giftcards) {
+		$coupons = array();
+	
+		foreach ($giftcards as $giftcard) {
+			$code = $giftcard['giftcard']->get_data()['code'];
+			$amount = $giftcard['amount'] * -1;
+			error_log('$code: ' . $code . ' $amount: ' . $amount);
+			$coupons[] = $this->create_gift_card("$this->name $code", $this->sku, $this->type, $amount);
+		}
+	
 		return $coupons;
 	}
 
